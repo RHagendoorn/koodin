424b8c3ebe9662708cefbc15bc8436c660fe79b5
Robbert Hagendoorn <robberthagendoorn@geodan.com>
2025-04-02 14:38:14 +0200

    Fix indentation

--- src/app/api/shows/mapper.ts
+++ src/app/api/shows/mapper.ts
@@ -1,37 +1,37 @@
 import { showData } from "./route"
 
 export interface MappedShowData {
-	score: number
-	img: string
-	name: string
-	id: number
+  score: number
+  img: string
+  name: string
+  id: number
 }
 
 function insertShow(genre: MappedShowData[] = [], insert: MappedShowData) {
-	const index = genre.findIndex(show => show.score < insert.score);
-	const insertAt = index === -1 ? genre.length : index;
+  const index = genre.findIndex(show => show.score < insert.score);
+  const insertAt = index === -1 ? genre.length : index;
 
-	return [
-		...genre.slice(0, insertAt),
-		insert,
-		...genre.slice(insertAt)
-	]
+  return [
+    ...genre.slice(0, insertAt),
+    insert,
+    ...genre.slice(insertAt)
+  ]
 }
 
 export function mapper(data: showData[]): Record<string, MappedShowData[]> {
-	return data
-		.filter(show => !!show.show.image?.medium)
-		.reduce((total, show) => {
-			const { image, name, id } = show.show
-			const showData = {
-				score: show.score,
-				img: image.medium!,
-				name,
-				id,
-			}
-			show.show.genres.forEach(genre => {
-				total[genre] = insertShow(total[genre], showData)
-			})
-			return total
-		}, {} as Record<string, MappedShowData[]>)
+  return data
+    .filter(show => !!show.show.image?.medium)
+    .reduce((total, show) => {
+      const { image, name, id } = show.show
+      const showData = {
+        score: show.score,
+        img: image.medium!,
+        name,
+        id,
+      }
+      show.show.genres.forEach(genre => {
+        total[genre] = insertShow(total[genre], showData)
+      })
+      return total
+    }, {} as Record<string, MappedShowData[]>)
 }
--- src/app/components/showlist.tsx
+++ src/app/components/showlist.tsx
@@ -2,29 +2,29 @@ import Link from "next/link";
 import { MappedShowData } from "../api/shows/mapper";
 
 interface ShowlistProps {
-	shows: MappedShowData[]
+  shows: MappedShowData[]
 }
 
 export function ShowList({ shows }: ShowlistProps) {
-	return (
-		<div className="space-y-4">
-			{
-				shows.map((show: any) => (
-					<Link href={`/shows/${show.id}`} key={show.id}>
-						<div key={show.id} className="flex items-center space-x-4 p-2 hover:bg-gray-100">
-							<img
-								src={show.img}
-								alt={show.name}
-								className="w-12 h-16 object-cover rounded-md"
-							/>
+  return (
+    <div className="space-y-4">
+      {
+        shows.map((show: any) => (
+          <Link href={`/shows/${show.id}`} key={show.id}>
+            <div key={show.id} className="flex items-center space-x-4 p-2 hover:bg-gray-100">
+              <img
+                src={show.img}
+                alt={show.name}
+                className="w-12 h-16 object-cover rounded-md"
+              />
 
-							<div>
-								<h3 className="text-xl font-semibold">{show.name}</h3>
-							</div>
-						</div>
-					</Link>
-				))
-			}
-		</div>
-	)
+              <div>
+                <h3 className="text-xl font-semibold">{show.name}</h3>
+              </div>
+            </div>
+          </Link>
+        ))
+      }
+    </div>
+  )
 }
